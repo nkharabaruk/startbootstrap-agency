9457921eb9f531ba0941edcd7ae9e6c1a5440c94
Andrii Stefanskyi <stefandriy@gmail.com>
2017-03-23 23:58:17 +0200

    i have no idea what was wrong, but now it works

--- mail/collect_data.php
+++ mail/collect_data.php
@@ -1,11 +1,18 @@
 <?php
-$info = strip_tags(htmlspecialchars($_POST['info']));
+$info = $_POST['info'];
+$info .= '';
+$info = str_replace("'", "", $info);
+$info = str_replace('"', "", $info);
+
+$len = strlen($info);
+$substr = substr($info, 0, $len - 1);
 
 $to = 'zapchastyny.sprinter@gmail.com';
-$email_subject = "zapchastyny-sprinter.com.ua - statistics";
-$email_body = "$info";
-$headers = "From: noreply@yourdomain.com\n";
-$headers .= "Reply-To: noreply@yourdomain.com\n";
-mail($to,$email_subject,$email_body,$headers);
-return true;
+$subject = "Wooo Email!";
+
+$message = "One new visitor! \r\n";
+$message .= "\r\n $substr";
+$message = wordwrap($message, 70);
+
+mail($to, $subject, $message, "From: system@yourdomain.com\r\n");
 ?>
