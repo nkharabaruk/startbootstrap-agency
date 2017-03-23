44629f1a09ece56900db94e4fef16001a0aa1a1a
Andrii Stefanskyi <stefandriy@gmail.com>
2017-03-23 21:37:20 +0200

    fix php variable

--- mail/collect_data.php
+++ mail/collect_data.php
@@ -3,7 +3,7 @@ $info = strip_tags(htmlspecialchars($_POST['info']));
 // Create the email and send the message
 $to = 'zapchastyny.sprinter@gmail.com';
 $email_subject = "zapchastyny-sprinter.com.ua - statistics";
-$email_body = info;
+$email_body = $info;
 $headers = "From: noreply@zapchastyny-sprinter.com.ua\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
